205a013ae415aa224b0a6b1b679bc1a0d09f10ca
Randy Soderstrom <rgsoderstrom@gmail.com>
2022-10-18 16:43:19 -0700

    unit test changes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 using CommonMath;
 
@@ -14,19 +15,16 @@ namespace CommonMathUT
         {
             try
             {
-                Matrix3x3 m33 = new Matrix3x3 ();
+                List<Point3D> source = new List<Point3D> () {new Point3D (0, 0, 0), 
+                                                             new Point3D (5, 0, 0),
+                                                             new Point3D (6, 0, 0),
+                                                             new Point3D (9, 0, 0)
+                };
 
-                m33.FillOneColumn (0, new List<double> {1, 2, 3});
-                m33.FillOneColumn (1, new List<double> {10, 20, 30});
-                m33.FillOneColumn (2, new List<double> {100, 200, 300});
+                List<Point3D> pts = Interpolation.Linear (source, 7);
 
-                ColumnVector v3 = new ColumnVector (3);
-
-                v3.FillByColumn (new double [] {23, 34, 56 });
-
-                ColumnVector v = new ColumnVector (m33 * v3);
-
-                Console.WriteLine ("{0:0.00}", v);
+                for (int i=0; i<pts.Count; i++)
+                    Console.WriteLine (i + ": " + pts [i].X + " " + pts [i].Y + " " + pts [i].Z);
             }
 
             catch (Exception ex)
