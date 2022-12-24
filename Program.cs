196906ffbd9cb59fef9913e6105a8b354f775e6d
Randy Soderstrom <rgsoderstrom@gmail.com>
2022-12-24 07:11:39 -0800

    Unit test for new "point" classes in CommonMath

--- Program.cs
+++ Program.cs
@@ -15,16 +15,22 @@ namespace CommonMathUT
         {
             try
             {
-                List<Point3D> source = new List<Point3D> () {new Point3D (0, 0, 0), 
-                                                             new Point3D (5, 0, 0),
-                                                             new Point3D (6, 0, 0),
-                                                             new Point3D (9, 0, 0)
-                };
+                // source points
+                PointXYZ car = new PointXYZ (-7, 5, 3);
+                PointSph sph = new PointSph (5, -30, 120);
+                PointCyl cyl = new PointCyl (3, -25, 9);
 
-                List<Point3D> pts = Interpolation.Linear (source, 7);
+                ICartesian src = sph;
 
-                for (int i=0; i<pts.Count; i++)
-                    Console.WriteLine (i + ": " + pts [i].X + " " + pts [i].Y + " " + pts [i].Z);
+                Console.WriteLine ("Source: " + src);
+
+                PointCyl s2 = new PointCyl (src);
+                Console.WriteLine ("Dest: " + s2);
+                
+                s2.Y += 2;
+                Console.WriteLine ("Dest: " + s2);
+
+
             }
 
             catch (Exception ex)
