da16dd346a7aa19424223fabd9a9a35524365cc9
Randy Soderstrom <rgsoderstrom@gmail.com>
2024-02-15 17:10:52 -0800

    Unit tests for Complex and ComplexMatrix

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
@@ -15,32 +16,74 @@ namespace CommonMathUT
         {
             try
             {
-                // source points
-                PointXYZ car1 = new PointXYZ (-7, 5, 3); // comment
-                PointSph sph1 = new PointSph (5, -30, 120);
-                PointCyl cyl1 = new PointCyl (3, -25, 9);
-
-                PointSph sph2 = new PointSph (7, -30, 120);
-
-                Vector3D v1 = sph2 - sph1;
-                Console.WriteLine (v1);
-
-                //ICartesian src = sph;
-
-                //Console.WriteLine ("Source: " + src);
-
-                //PointCyl s2 = new PointCyl (src);
-                //Console.WriteLine ("Dest: " + s2);
+                CMatrix cm1 = new CMatrix (2, 2);
+                CMatrix cm2 = new CMatrix (4, 4);
                 
-                //s2.Y += 2;
-                //Console.WriteLine ("Dest: " + s2);
+                Complex [] cm1Data = new Complex [] // data copied from Matlab
+                {
+                    new Complex ("   0.6897 +  0.2305i"), 
+                    new Complex ("   0.9545 +  0.7410i"),
+                    new Complex ("   0.1721 +  0.3898i"),
+                    new Complex ("   0.2276 +  0.7689i"),
+                };
+
+                Complex [] cm2Data = new Complex [] // data copied from Matlab
+                {
+                    new Complex ("  0.1522 +  0.6552i"),
+                    new Complex ("   0.7623 +  0.9862i"),
+                    new Complex ("   0.9112 +  0.6565i"),
+                    new Complex ("   0.4618 +  0.9132i"),
+                    new Complex ("   0.2661 +  0.1018i"),
+                    new Complex ("   0.0484 +  0.0395i"),
+                    new Complex ("   0.3236 +  0.5196i"),
+                    new Complex ("   0.0204 +  0.2899i"), 
+                    new Complex ("   0.7189 +  0.2515i"),
+                    new Complex ("   0.9445 +  0.4942i"),
+                    new Complex ("   0.1273 +  0.9954i"),
+                    new Complex ("   0.0756 +  0.3774i"),
+                    new Complex ("   0.7457 +  0.3774i"),
+                    new Complex ("   0.0089 +  0.4399i"),
+                    new Complex ("   0.5407 +  0.0840i"),
+                    new Complex ("   0.4398 +  0.6215i") 
+                };
+
+                cm1.FillByColumn (cm1Data);
+                cm2.FillByColumn (cm2Data);
 
 
+                //for (int i=0; i<cm1.Rows; i++)
+                //    for (int j=0; j<cm1.Cols; j++)
+                //        cm1 [i, j] = new Complex (i + 3, j + 2);
+
+                //for (int i=0; i<cm2.Rows; i++)
+                //    for (int j=0; j<cm2.Cols; j++)
+                //        cm2 [i, j] = new Complex (i + 1, j + 1);
+
+                List<CMatrix> results = new List<CMatrix> ()
+                {
+                    cm1,
+                    cm2,
+                    //cm1 * cm2,
+                    //cm2 * cm1,
+                    //cm1 * CMatrix.Transpose (cm1),
+                    CMatrix.Inverse (cm1),
+                    CMatrix.Inverse (cm2),
+                    CMatrix.Inverse (cm1) * cm1,
+                    CMatrix.Inverse (cm2) * cm2,
+                };
+
+                foreach (CMatrix m in results)
+                {
+                    Console.WriteLine (m.GetType ());
+                    Console.WriteLine (m);
+                    Console.WriteLine ();
+                }
             }
 
             catch (Exception ex)
             {
                 Console.WriteLine ("Exception: " + ex.Message);
+                Console.WriteLine ("Exception: " + ex.StackTrace);
             }
         }
     }
