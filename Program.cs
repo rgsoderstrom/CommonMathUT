bb487346d21e3f66febad8b056e05c3af557f49f
Randy Soderstrom <rgsoderstrom@gmail.com>
2024-02-18 14:15:13 -0800

    New unit tests for complex operatons

--- Program.cs
+++ Program.cs
@@ -16,6 +16,11 @@ namespace CommonMathUT
         {
             try
             {
+                Complex c1 = new Complex (1, 2);
+                Complex c2 = new Complex (3, 4);
+                Complex c3 = c1 + c2;
+
+
                 CMatrix cm1 = new CMatrix (2, 2);
                 CMatrix cm2 = new CMatrix (4, 4);
                 
@@ -29,27 +34,26 @@ namespace CommonMathUT
 
                 Complex [] cm2Data = new Complex [] // data copied from Matlab
                 {
-                    new Complex ("  0.1522 +  0.6552i"),
-                    new Complex ("   0.7623 +  0.9862i"),
-                    new Complex ("   0.9112 +  0.6565i"),
-                    new Complex ("   0.4618 +  0.9132i"),
-                    new Complex ("   0.2661 +  0.1018i"),
-                    new Complex ("   0.0484 +  0.0395i"),
-                    new Complex ("   0.3236 +  0.5196i"),
-                    new Complex ("   0.0204 +  0.2899i"), 
-                    new Complex ("   0.7189 +  0.2515i"),
-                    new Complex ("   0.9445 +  0.4942i"),
-                    new Complex ("   0.1273 +  0.9954i"),
-                    new Complex ("   0.0756 +  0.3774i"),
-                    new Complex ("   0.7457 +  0.3774i"),
-                    new Complex ("   0.0089 +  0.4399i"),
-                    new Complex ("   0.5407 +  0.0840i"),
-                    new Complex ("   0.4398 +  0.6215i") 
+                    new Complex ("  0.3759 +  0.8670i"),
+                    new Complex ("  0.0183 +  0.2714i"),
+                    new Complex ("  0.9134 +  0.2174i"),
+                    new Complex ("  0.3580 +  0.6897i"),
+                    new Complex ("  0.7604 +  0.2305i"),
+                    new Complex ("  0.8077 +  0.1721i"),
+                    new Complex ("  0.0990 +  0.3898i"),
+                    new Complex ("  0.4972 +  0.9545i"), 
+                    new Complex ("  0.3478 +  0.7410i"),
+                    new Complex ("  0.0276 +  0.2276i"),
+                    new Complex ("  0.5313 +  0.7689i"),
+                    new Complex ("  0.9958 +  0.1522i"),
+                    new Complex ("  0.2079 +  0.6552i"),
+                    new Complex ("  0.4921 +  0.2661i"),
+                    new Complex ("  0.7597 +  0.1018i"),
+                    new Complex ("  0.3365 +  0.7189i") 
                 };
 
                 cm1.FillByColumn (cm1Data);
                 cm2.FillByColumn (cm2Data);
-
 
                 //for (int i=0; i<cm1.Rows; i++)
                 //    for (int j=0; j<cm1.Cols; j++)
@@ -61,15 +65,16 @@ namespace CommonMathUT
 
                 List<CMatrix> results = new List<CMatrix> ()
                 {
-                    cm1,
-                    cm2,
-                    //cm1 * cm2,
+                    //cm1,
+                    //cm2 / 3,
+                    new Complex (3, 2) / cm2,  
+                    //cm2 / new Complex (3, 2),
                     //cm2 * cm1,
                     //cm1 * CMatrix.Transpose (cm1),
-                    CMatrix.Inverse (cm1),
-                    CMatrix.Inverse (cm2),
-                    CMatrix.Inverse (cm1) * cm1,
-                    CMatrix.Inverse (cm2) * cm2,
+                    //CMatrix.Inverse (cm1),
+                    //CMatrix.Inverse (cm2),
+                    //CMatrix.Inverse (cm1) * cm1,
+                    //CMatrix.Inverse (cm2) * cm2,
                 };
 
                 foreach (CMatrix m in results)
